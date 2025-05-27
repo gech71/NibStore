872b4c79196a8d444134d92dc0635e33d44d8b7a
Robel Asaminew <Robel.Asaminew@nibbanksc.com>
2025-05-27 15:34:12 +0300

    Merchant Store

---
+++ src/Smartstore.Web/Areas/Admin/Models/MerchantStores/MerchantStoreModel.cs
@@ -0,0 +1,37 @@
+using Smartstore.Core.Content.Media;
+using System.ComponentModel.DataAnnotations;
+namespace Smartstore.Admin.Models.MerchantStores
+{
+    [LocalizedDisplay("Admin.Configuration.MerchantStores.Fields.")]
+    public class MerchantStoreModel : EntityModelBase
+    {
+        [LocalizedDisplay("*Name")]
+        public string Name { get; set; }
+
+        [LocalizedDisplay("*Description")]
+        public string Description { get; set; }
+
+        [LocalizedDisplay("Common.DisplayOrder")]
+        public int DisplayOrder { get; set; }
+
+        [LocalizedDisplay("*Address")]
+        public string Address { get; set; }
+
+        [LocalizedDisplay("*OpeningHours")]
+        public string OpeningHours { get; set; }
+
+        [LocalizedDisplay("*OpeningTime")]
+        public string OpeningTime { get; set; }
+
+        [LocalizedDisplay("*ClosingTime")]
+        public string ClosingTime { get; set; }
+
+        [LocalizedDisplay("*Published")]
+        public bool Published { get; set; }
+
+        [LocalizedDisplay("*Picture")]
+        [UIHint("Media")]
+        [AdditionalMetadata("album", "content")]
+        public int? MediaFileId { get; set; }
+    }
+}
