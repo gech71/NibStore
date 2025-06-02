66006fc989b6e07c81d07a214b3cbbb6b94e82e4
Tadelachewu <tade2024bdu@gmail.com>
2025-06-02 17:07:12 +0300

    real stores for in store shipping

---
+++ src/Smartstore.Core/Common/Services/StoreProductService.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Smartstore.Core.Data;
+using Smartstore.Core.Content.MerchantStores;
+
+namespace Smartstore.Core.Common.Services
+{
+    public class StoreProductService : IStoreProductService
+    {
+        private readonly SmartDbContext _db;
+
+        public StoreProductService(SmartDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IList<string>> GetStoresForProductAsync(int productId)
+        {
+            var storeNames = await _db.Set<ProductMerchantStoreMapping>()
+                .Include(p => p.MerchantStore)
+                .Where(p => p.ProductId == productId && p.MerchantStore.Published)
+                .OrderBy(p => p.MerchantStore.DisplayOrder)
+                .Select(p => p.MerchantStore.Name)
+                .ToListAsync();
+
+            return storeNames;
+        }
+
+        public async Task<IList<MerchantStore>> GetStoreDetailsForProductAsync(int productId)
+        {
+            var stores = await _db.Set<ProductMerchantStoreMapping>()
+                .Include(p => p.MerchantStore)
+                .Where(p => p.ProductId == productId && p.MerchantStore.Published)
+                .OrderBy(p => p.MerchantStore.DisplayOrder)
+                .Select(p => p.MerchantStore)
+                .ToListAsync();
+
+            return stores;
+        }
+    }
+}
