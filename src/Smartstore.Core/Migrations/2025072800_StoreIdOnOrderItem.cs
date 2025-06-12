780d5bdb9f2f68b93f84b90cc39b983a4687e4d4
Robel Asaminew <Robel.Asaminew@nibbanksc.com>
2025-06-12 15:55:24 +0300

    storeid migration update

--- src/Smartstore.Core/Migrations/2025072800_StoreIdOnOrderItem.cs
+++ src/Smartstore.Core/Migrations/2025072800_StoreIdOnOrderItem.cs
@@ -3,14 +3,37 @@ using Smartstore.Core.Data.Migrations;
 
 namespace Smartstore.Core.Migrations
 {
-    [MigrationVersion("2025-06-07 01:00:00", "Add StorePickupId column to OrderItem")]
+    [MigrationVersion("2025-06-07 01:00:00", "Add StorePickupId and StoreName columns to OrderItem")]
     public class AddStorePickupIdToOrderItem : Migration
     {
         public override void Up()
         {
-            Alter.Table("OrderItem")
-                .AddColumn("StorePickupId").AsInt32().Nullable()
-                .AddColumn("StoreName").AsString(400).Nullable();
+            IfDatabase("postgres")
+                .Execute.Sql(@"
+                    DO $$
+                    BEGIN
+                        -- Add StorePickupId column if it does not exist
+                        IF NOT EXISTS (
+                            SELECT 1
+                            FROM information_schema.columns
+                            WHERE table_name = 'OrderItem'
+                              AND column_name = 'StorePickupId'
+                        ) THEN
+                            ALTER TABLE ""OrderItem"" ADD COLUMN ""StorePickupId"" INTEGER;
+                        END IF;
+
+                        -- Add StoreName column if it does not exist
+                        IF NOT EXISTS (
+                            SELECT 1
+                            FROM information_schema.columns
+                            WHERE table_name = 'OrderItem'
+                              AND column_name = 'StoreName'
+                        ) THEN
+                            ALTER TABLE ""OrderItem"" ADD COLUMN ""StoreName"" VARCHAR(400);
+                        END IF;
+                    END;
+                    $$;
+                ");
         }
 
         public override void Down()
